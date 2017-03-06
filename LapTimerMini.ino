fe24a425d9ebda650a351edc7f0fa08181348c58
rgmorales <rgmorales@gmail.com>
2017-03-05 19:23:12 -0600

    Updated logic to only refresh screen after RMC
    Found out that each time I refresh the OLED screen, it interferes with the serial reading...
    Now we only refresh the screen after speed is updated, which is when the RMC string is processed. 
    Only 2 strings are processed, the RCG and RMC. By updating the screen only when the speed is updated, makes sure the RMC string is processed.

--- LapTimerMini.ino
+++ LapTimerMini.ino
@@ -1,13 +1,13 @@
+#include <TinyGPS++.h>
 #include "SSD1306Ascii.h"
 #include "SSD1306AsciiAvrI2c.h"
-#include <TinyGPS++.h>
-#include <Time.h>
-#include <SoftwareSerial.h>
+
 
 // 0X3C+SA0 - 0x3C or 0x3D
 #define I2C_ADDRESS 0x3C
 
 SSD1306AsciiAvrI2c oled;
+
 
 #define PMTK_SET_BAUD_57600 "$PMTK251,57600*2C"
 // turn on GPRMC and GGA
@@ -15,15 +15,10 @@ SSD1306AsciiAvrI2c oled;
 #define PMTK_SET_NMEA_UPDATE_10HZ "$PMTK220,100*2F"
 #define PGCMD_NOANTENNA "$PGCMD,33,0*6D" 
 
-//pins used by software serial
-static const int RXPin = 4, TXPin = 3;
-
 // The TinyGPS++ object
 TinyGPSPlus gps;
 
-
-// The serial connection to the GPS device
-SoftwareSerial ss(RXPin, TXPin);
+#define serial1 Serial1
 
 //if we have an active session going on, we must do the laps tracking
 bool sessionActive = false;
@@ -31,117 +26,135 @@ bool sessionActive = false;
 unsigned long sessionTime = 0;
 unsigned long bestSessionTime = 0;
 
+unsigned long currentLap = 0;
+unsigned long bestLap = 0;
+
 //previous position measured
-double previousLat = 0.0;
-double previousLon = 0.0;
+float previousLat = 0.0;
+float previousLng = 0.0;
 //From the track, the finishing line
-double finishLineLat1 = 33.044556; 
-double finishiLineLon1 = -96.682413;
-double finishLineLat2 = 33.044540;
-double finishLineLon2 = -96.682239;
+float finishLineLat1 = 33.044556;
+float finishiLineLon1 = -96.682413;
+float finishLineLat2 = 33.044540;
+float finishLineLon2 = -96.682239;
 
 unsigned long lapComplete = 0;
 
+
+
 void setup()
 {
-	Serial.begin(115200);	
+
+	while (!Serial);
+	while (!Serial1);
+
+	Serial.begin(57600);	
 
 	oled.begin(&Adafruit128x64, I2C_ADDRESS);
 	oled.setFont(Adafruit5x7);
-	//oled.setFont(Stang5x7);	
+	oled.setFont(Stang5x7);	
 
 	initializeGPS();
 
 	waitForFix();
 	
 	oled.clear();
-	updateScreen(0, 0, 0, 0);
+	
 
 }
 
 void loop()
 {
-	// This sketch displays information every time a new sentence is correctly encoded.
-	while (ss.available() > 0) {
+	
+	while (serial1.available() > 0) {
 		//the GPS is configured to 10Hz, this if will happen 10 times per second
-		if (gps.encode(ss.read())) {			
+		if (gps.encode(serial1.read())) {
 
-			if ((previousLat != gps.location.lat()) || (previousLon != gps.location.lng())) {
+			//the GPS is configured to 10Hz, this if will happen 10 times per second
+
+			if ((previousLat != gps.location.lat()) || (previousLng != gps.location.lng())) {
 				//new lap, should log the current lap, check for best lap, and give the lap timer 5 seconds to move to next position
-				if (segmentsIntersect(gps.location.lat(), gps.location.lng(), previousLat, previousLon, finishLineLat1, finishiLineLon1, finishLineLat2, finishLineLon2) && (lapComplete > 50)) {					
-					//reset lap complete, for the next lap
-					lapComplete = 0;
-					
-					if (!sessionActive)  sessionActive = true;
+				if (segmentsIntersect(gps.location.lat(), gps.location.lng(), previousLat, previousLng, finishLineLat1, finishiLineLon1, finishLineLat2, finishLineLon2)) {			
 
-					if (bestSessionTime == 0) bestSessionTime = millis() - sessionTime;
-
-					if (bestSessionTime > millis() - sessionTime) bestSessionTime = millis() - sessionTime;
-					
+					//Session starting, this is the first lap
+					if (!sessionActive) {
+						sessionActive = true;
+						sessionTime = millis();
+					}
+					else {						
+						currentLap += 1;
+						//this is the best or first lap
+						if ((bestSessionTime > millis() - sessionTime) || (bestSessionTime==0)) {//test for session time and also for the very first lap, when bestSesstionTime is 0
+							bestSessionTime = millis() - sessionTime;
+							bestLap = currentLap;
+						}						
+					}
 					//reset the sessionTime
 					sessionTime = millis();
 				}
+
 			}
+				
+				
 
 			previousLat = gps.location.lat();
-			previousLat = gps.location.lng();
+			previousLng = gps.location.lng();
 
-			if (sessionActive) updateScreen(gps.satellites.value(), gps.speed.mph(), millis()-sessionTime, bestSessionTime);
-			else updateScreen(gps.satellites.value(), gps.speed.mph(), 0,0);
-
-			//used to control 5 seconds interval every time a lap is completed
-			lapComplete += 1;
+			if (gps.speed.isUpdated()) {
+				if (sessionActive) updateScreen((int)gps.satellites.value(), gps.speed.mph(), millis() - sessionTime, bestSessionTime, bestLap);
+				else updateScreen((int)gps.satellites.value(), gps.speed.mph(), 0, 0, 0);
+			}				
 		}
 	}
 			
 }
 
-
-
 void initializeGPS() {
 	oled.clear();
 	oled.setContrast(255);
 	oled.println(F("Initializing GPS..."));
+	oled.set2X();
 	oled.println(F(""));
-	oled.println(F("Config baud rate"));
+	oled.print(F("*"));
 	// If GPS has the default settings, we need to change the BAUD rate to 57600
-	ss.begin(9600);
-	ss.println(F(PMTK_SET_BAUD_57600));
+	serial1.begin(9600);
+	serial1.println(F(PMTK_SET_BAUD_57600));
 	delay(500);//wait for the command to take effect  
-	//we need to close and open the GPS with the new baud rate
-	ss.end();
+			   //we need to close and open the GPS with the new baud rate
+	serial1.end();
 	delay(500);
-	ss.begin(57600);
+	serial1.begin(57600);
 	delay(500);
-	oled.println(F("Config NMEA"));
+	oled.print(F("*"));
 	//Turn on RMC (recommended minimum) and GGA (fix data) including altitude
-	ss.println(F(PMTK_SET_NMEA_OUTPUT_RMCGGA));
+	serial1.println(F(PMTK_SET_NMEA_OUTPUT_RMCGGA));
 	delay(500);
 	// uncomment this line to turn on only the "minimum recommended" data
 	// Set the update rate
-	oled.println(F("Config 10hz"));
-	ss.println(F(PMTK_SET_NMEA_UPDATE_10HZ));   // 10 Hz update rate
+	oled.print(F("*"));
+	serial1.println(F(PMTK_SET_NMEA_UPDATE_10HZ));   // 10 Hz update rate
 	delay(500);
 	// Request updates on antenna status, comment out to keep quiet
-	oled.println(F("Config no antenna "));
-	ss.println(F(PGCMD_NOANTENNA));//No antena
-	delay(500);	
-	oled.println(F("Config finished!!!"));
+	oled.print(F("*"));
+	serial1.println(F(PGCMD_NOANTENNA));//No antena
+	delay(500);
+	oled.print(F("*"));
 	delay(2000);
 }
 
-void updateScreen(uint32_t numSats,double speed,unsigned long currentSession, unsigned long bestSession) {
+void updateScreen(uint8_t numSats,double speed,unsigned long currentSession, unsigned long bestSession, unsigned long bestLap) {
+	if (numSats < 3){
+		waitForFix();
+		return;
+	}
 	oled.home();
 	oled.set1X();
-	if (numSats != 0) oled.println("Sats:"+ String(numSats,0)+" - " +String(speed,0) + " mph     ");
-	else waitForFix();
-	oled.println();		
-	oled.println(F("Best:"));
-	//oled.println();
+	oled.println("Sats:" + String((int)numSats) + " - " + String(speed,0) + " mph     ");	
+	oled.println((((millis() / 100) % 10) % 2==0)?"       ***          ":"                  ");
+	oled.println("Best:" + String(bestLap));	
 	oled.set2X();
-	oled.println(formatSessionTime(bestSession));
-	oled.set1X();
-	//oled.println();
+	oled.println(formatSessionTime(bestSession) +"         ");
+	oled.set1X();	
 	oled.println(F("Current:"));
 	oled.set2X();
 	oled.println(formatSessionTime(currentSession)+ "        ");	
@@ -176,37 +189,28 @@ boolean segmentsIntersect(float lat1, float  lon1, float  lat2, float  lon2, flo
 	return (s >= 0) && (s <= 1) && (t >= 0) && (t <= 1);
 }
 
-void waitForFix() {
-	int count = 1;
+void waitForFix() {	
 	oled.clear();			
 	// This sketch displays information every time a new sentence is correctly encoded.
-	do{
-		oled.home();
-		oled.set1X();
-		oled.println(F("Waiting for Sats Fix."));
-		oled.println(F(""));
-		oled.println(F("3 Sats needed."));
-		oled.println(F("Current Sats:"));
-		oled.println(F(""));
-		oled.set2X();
-		oled.print("    " + String(gps.satellites.value()));
-		oled.set1X();
-		oled.print(" (" + String(count) + "s)");
-		oled.set1X();
-		if (gps.charsProcessed()>0)	count += 1;
-		smartDelay(1000);
-	} while ((gps.satellites.value() < 3)|| (count<3));
+	do{	
+		while (serial1.available() > 0) 			
+			if (gps.encode(serial1.read())) {
+				oled.home();
+				oled.set1X();
+				oled.println(F("Waiting for Sats Fix."));
+				oled.println(F(""));
+				oled.println(F("3 Sats needed."));
+				oled.println(F("Current Sats:"));
+				oled.println(F(""));
+				oled.set2X();
+				oled.print("    " + String((int)gps.satellites.value()));
+				oled.set1X();
+				oled.set1X();
+				oled.println((((millis() / 100) % 10) % 2 == 0) ? "  ***          " : "                  ");
+			}
+	} while ((int)gps.satellites.value() < 3);
+	delay(3000);
 	oled.clear();
 }
 
-// This custom version of delay() ensures that the gps object
-// is being "fed".
-static void smartDelay(unsigned long ms)
-{
-	unsigned long start = millis();
-	do
-	{
-		while (ss.available())
-			gps.encode(ss.read());
-	} while (millis() - start < ms);
-}
+
